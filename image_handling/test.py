921aee8d1d76f61df20fbf3ac6530f3ff415639b
Siddharth <st22041@ormiston.school.nz>
2023-01-01 20:16:00 +1300

    Add pytesseract

--- image_handling/test.py
+++ image_handling/test.py
@@ -1,6 +1,6 @@
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\sidsa\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts\pytesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR/tesseract.exe'
 
 print(pytesseract.image_to_string(Image.open("images/test.jpg")))
