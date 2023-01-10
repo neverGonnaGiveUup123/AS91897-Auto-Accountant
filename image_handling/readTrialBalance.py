3ba73c5edaf78225a07d3aa6ea8ae8f91d46cd4a
Siddharth <st22041@ormiston.school.nz>
2023-01-10 15:42:38 +1300

    Add readTrialBalance

---
+++ image_handling/readTrialBalance.py
@@ -0,0 +1,15 @@
+import pytesseract
+from PIL import Image
+
+class readTrialBalance:
+    pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR/tesseract.exe'
+
+    def __init__(self, image: str) -> None:
+        self.image = image
+
+    def splitStringTrialBalance(self) -> list:
+        trialBalance = pytesseract.image_to_string(Image.open(self.image))
+        return trialBalance.split()
+
+t = readTrialBalance('images/test.jpg')
+print(t.splitStringTrialBalance())
