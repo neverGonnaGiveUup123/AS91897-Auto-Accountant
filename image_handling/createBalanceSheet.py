0cd5c4fbff0e3e9a19acd0ef2cd85388f8183dce
Siddharth <st22041@ormiston.school.nz>
2023-01-12 16:56:48 +1300

    Layed foundation for objects, this version does not work

--- image_handling/createBalanceSheet.py
+++ image_handling/createBalanceSheet.py
@@ -3,10 +3,26 @@ import pandas as pd
 import os
 from readTrialBalance import readTrialBalance
 
-trialBalance = readTrialBalance('images/test.jpg')
-splitText = trialBalance.splitStringTrialBalance()
+# trialBalance = readTrialBalance('images/test.jpg')
+# splitText = trialBalance.splitStringTrialBalance()
 
-print(splitText)
+# print(splitText)
+
+class createBalanceSheet:
+    def __init__(self,trialBalance,splitText) -> None:
+        self.trialBalance = trialBalance
+        self.splitText = splitText
+    
+    def set_dict_values(self) -> None:
+        for items in self.splitText:
+            pass
+    
+    def find_values():
+        key_words = []
+        jumps = []
+        for i in zip(key_words,jumps):
+            pass
+
 
 currentAssets = {
     'Bank' : 0,
@@ -20,25 +36,25 @@ investments = {
     'shares' : 0,
 }
 
-def get_num(dictReference, dictKey, specifyJump):
-    newValue = splitText[splitText.index(i) + specifyJump].replace(',', '')
-    dictReference[str(dictKey)] = int(newValue.replace('$', ''))
+# def get_num(dictReference, dictKey, specifyJump):
+#     newValue = splitText[splitText.index(i) + specifyJump].replace(',', '')
+#     dictReference[str(dictKey)] = int(newValue.replace('$', ''))
 
-def check_trial_balance():
-    for j in currentAssets.keys():
-        if re.search(j, i) != None:
-            get_num(currentAssets, j, 1)
-    if re.search('Receivable', i) != None:
-        get_num(currentAssets, "Accounts Receivable",1)
-    if re.search('Income', i) != None:
-        get_num(currentAssets, "Accrued Income",1)
-    if re.search("hand", i) != None:
-        get_num(currentAssets, "Inventory on hand",5)
-    if re.search("Shares", i) != None:
-        get_num(investments, "Shares", 3)
+# def check_trial_balance():
+#     for j in currentAssets.keys():
+#         if re.search(j, i) != None:
+#             get_num(currentAssets, j, 1)
+#     if re.search('Receivable', i) != None:
+#         get_num(currentAssets, "Accounts Receivable",1)
+#     if re.search('Income', i) != None:
+#         get_num(currentAssets, "Accrued Income",1)
+#     if re.search("hand", i) != None:
+#         get_num(currentAssets, "Inventory on hand",5)
+#     if re.search("Shares", i) != None:
+#         get_num(investments, "Shares", 3)
 
-for i in splitText:
-    check_trial_balance()
+# for i in splitText:
+#     check_trial_balance()
     
 print(currentAssets)
 
